89fa59b9985bef93615c3c8b95aebd3f8c470fc2
disha-wani <disha.wani@WSD008>
2018-06-09 18:43:39 +0530

    add RTS stack-resource datasource

---
+++ opentelekomcloud/data_source_opentelekomcloud_rts_stack_resource_v1.go
@@ -0,0 +1,102 @@
+package opentelekomcloud
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/huaweicloud/golangsdk/openstack/rts/v1/stackresources"
+	"log"
+)
+
+func dataSourceRTSStackResourcesV1() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRTSStackResourcesV1Read,
+
+		Schema: map[string]*schema.Schema{
+			"region": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"stack_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"stack_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"resource_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"logical_resource_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"required_by": &schema.Schema{
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+			"resource_status": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"resource_status_reason": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"physical_resource_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"resource_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func dataSourceRTSStackResourcesV1Read(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	orchestrationClient, err := config.orchestrationV1Client(GetRegion(d, config))
+
+	listOpts := stackresources.ListOpts{
+		Name:       d.Get("resource_name").(string),
+		LogicalID:  d.Get("logical_resource_id").(string),
+		PhysicalID: d.Get("physical_resource_id").(string),
+		Status:     d.Get("resource_status").(string),
+		Type:       d.Get("resource_type").(string),
+	}
+
+	refinedResources, err := stackresources.List(orchestrationClient, d.Get("stack_name").(string), d.Get("stack_id").(string), listOpts)
+	log.Printf("[INFO] Value of allResources: %#v", refinedResources)
+	if err != nil {
+		return fmt.Errorf("Unable to retrieve Resources: %s", err)
+	}
+
+	if refinedResources == nil || len(refinedResources) == 0 {
+		return fmt.Errorf("No matching resource found. " +
+			"Please change your search criteria and try again.")
+	}
+
+	if len(refinedResources) > 1 {
+		return fmt.Errorf("multiple resources matched; use additional constraints to reduce matches to a single resource")
+	}
+
+	resources := refinedResources[0]
+	d.SetId(resources.Name)
+
+	d.Set("resource_name", resources.Name)
+	d.Set("resource_status", resources.Status)
+	d.Set("logical_resource_id", resources.LogicalID)
+	d.Set("physical_resource_id", resources.PhysicalID)
+	d.Set("required_by", resources.RequiredBy)
+	d.Set("resource_status_reason", resources.StatusReason)
+	d.Set("resource_type", resources.Type)
+	d.Set("region", GetRegion(d, config))
+	return nil
+}
--- opentelekomcloud/provider.go
+++ opentelekomcloud/provider.go
@@ -164,6 +164,7 @@ func Provider() terraform.ResourceProvider {
 			"opentelekomcloud_kms_data_key_v1":        dataSourceKmsDataKeyV1(),
 			"opentelekomcloud_rds_flavors_v1":         dataSourceRdsFlavorV1(),
 			"opentelekomcloud_rts_stack_v1":           dataSourceRTSStackV1(),
+			"opentelekomcloud_rts_stack_resource_v1":  dataSourceRTSStackResourcesV1(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
