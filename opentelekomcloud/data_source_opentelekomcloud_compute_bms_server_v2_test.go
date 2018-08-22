3c618b555fea0e4ed01681142e101e73989c6b0a
abhijeet.choudhari <achoudhari10@gmail.com>
2018-08-22 17:57:50 +0530

    changes in acc test

--- opentelekomcloud/data_source_opentelekomcloud_compute_bms_server_v2_test.go
+++ opentelekomcloud/data_source_opentelekomcloud_compute_bms_server_v2_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestAccOTCBMSServerV2DataSource_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheckBMSServer(t) },
+		PreCheck:  func() { testAccPreCheckRequiredEnvVars(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
@@ -41,7 +41,21 @@ func testAccCheckBMSServerV2DataSourceID(n string) resource.TestCheckFunc {
 }
 
 var testAccOTCBMSServerV2DataSource_basic = fmt.Sprintf(`
-data "opentelekomcloud_compute_bms_server_v2" "server1" {
-  id = "%s"  
+resource "opentelekomcloud_compute_instance_v2" "instance_1" {
+  name = "BMSinstance_1"
+  image_id = "%s"
+  security_groups = ["default"]
+  availability_zone = "%s"
+  flavor_id = "physical.o2.medium"
+  metadata {
+    foo = "bar"
+  }
+  network {
+    uuid = "%s"
+  }
 }
-`, OS_SERVER_ID)
+
+data "opentelekomcloud_compute_bms_server_v2" "server1" {
+  id = "${opentelekomcloud_compute_instance_v2.instance_1.id}"
+}
+`, OS_IMAGE_ID,OS_AVAILABILITY_ZONE,OS_NETWORK_ID)
