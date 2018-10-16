b925e9299a7a20d3d7ef379d6563ef0b32ebbe13
aashish.mohankar <aashish.mohankar@click2cloud.net>
2018-10-16 18:46:01 +0530

    remove repeated resource name in positive testscripts .tf file.

--- positive_testscripts/modifySfsUsingMandatoryParameter.tf
+++ positive_testscripts/modifySfsUsingMandatoryParameter.tf
@@ -8,9 +8,3 @@ resource "opentelekomcloud_sfs_file_system_v2" "Share_file_002" {
   access_to = "${opentelekomcloud_vpc_v1.vpc_sfs002.id}"
   access_level = "rw"
 }
-resource "opentelekomcloud_sfs_file_system_v2" "Share_file_002" {
-  size = 50
-  name = "test-share-02"
-  access_to = "${opentelekomcloud_vpc_v1.vpc_sfs002.id}"
-  access_level = "rw"
-}
