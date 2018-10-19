c11ef1e55161b13d1f1204e2ae7d8b6d03000010
aashish.mohankar <aashish.mohankar@click2cloud.net>
2018-10-19 17:31:23 +0530

    remove vpc resource in modify-sfs.tf file.

--- modify-resource_testscripts/modify-sfs.tf
+++ modify-resource_testscripts/modify-sfs.tf
@@ -1,8 +1,4 @@
-resource "opentelekomcloud_vpc_v1" "vpc_sfs011" {
-  name = "vpc_sfs_test11"
-  cidr="192.168.0.0/16"
-}
-resource "opentelekomcloud_sfs_file_system_v2" "Share_file_011" {
+resource "${opentelekomcloud_sfs_file_system_v2.Share_file_011.id}" {
   size = 60
   name = "test-share1111"
   access_to = "${opentelekomcloud_vpc_v1.vpc_sfs011.id}"
