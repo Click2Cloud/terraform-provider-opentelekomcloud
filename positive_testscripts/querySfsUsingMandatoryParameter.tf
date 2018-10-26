35f726ee8069413b8bcaf91053b283dc02dd0729
aashish.mohankar <aashish.mohankar@click2cloud.net>
2018-10-26 15:00:24 +0530

    Changes in querySfsUsingMandatoryParameter.tf file.

--- positive_testscripts/querySfsUsingMandatoryParameter.tf
+++ positive_testscripts/querySfsUsingMandatoryParameter.tf
@@ -30,9 +30,9 @@ output "availability_zone" {
 output "size" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.size}"
 }
-output "share_type" {
+/*output "share_type" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.share_type}"
-}
+}*/
 output "status" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.status}"
 }
@@ -51,7 +51,7 @@ output "name" {
 output "share_proto" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.share_proto}"
 }
-output "volume_type" {
+/*output "volume_type" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.volume_type}"
 }
 output "export_location" {
@@ -59,16 +59,16 @@ output "export_location" {
 }
 output "metadata" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.metadata}"
-}
-output "export_locations" {
+}*/
+/*output "export_locations" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.export_locations}"
-}
+}*/
 output "access_level" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.access_level}"
 }
-output "state" {
+/*output "state" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.state}"
-}
+}*/
 output "share_access_id" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.share_access_id}"
 }
@@ -78,12 +78,13 @@ output "access_type" {
 output "access_to" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.access_to}"
 }
-output "mount_id" {
+/*output "mount_id" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.mount_id}"
 }
 output "share_instance_id" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.share_instance_id}"
 }
+
 output "preferred" {
   value = "${opentelekomcloud_sfs_file_system_v2.Share_file_004.preferred}"
-}
+}*/
