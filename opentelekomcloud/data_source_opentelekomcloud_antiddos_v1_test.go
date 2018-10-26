b70748b90d127d9d0c9d8d979d32d36d030ea4f8
d-apurva <apurva.dhoble@click2cloud.net>
2018-10-26 11:42:10 +0530

    update acc test for datasource

--- opentelekomcloud/data_source_opentelekomcloud_antiddos_v1_test.go
+++ opentelekomcloud/data_source_opentelekomcloud_antiddos_v1_test.go
@@ -17,7 +17,7 @@ func TestAccAntiDdosV1DataSource_basic(t *testing.T) {
 				Config: testAccAntiDdosV1DataSource_basic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAntiDdosV1DataSourceID("data.opentelekomcloud_antiddos_v1.antiddos"),
-					resource.TestCheckResourceAttr("data.opentelekomcloud_antiddos_v1.antiddos", "floating_ip_id", "160.44.206.31"),
+					resource.TestCheckResourceAttr("data.opentelekomcloud_antiddos_v1.antiddos", "network_type", "EIP"),
 					resource.TestCheckResourceAttr("data.opentelekomcloud_antiddos_v1.antiddos", "status", "normal"),
 				),
 			},
@@ -29,11 +29,11 @@ func testAccCheckAntiDdosV1DataSourceID(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return fmt.Errorf("Can't find backup data source: %s ", n)
+			return fmt.Errorf("Can't find defense status of EIP data source: %s ", n)
 		}
 
 		if rs.Primary.ID == "" {
-			return fmt.Errorf("backup data source ID not set ")
+			return fmt.Errorf("Defense status of EIP data source ID not set")
 		}
 
 		return nil
@@ -41,7 +41,28 @@ func testAccCheckAntiDdosV1DataSourceID(n string) resource.TestCheckFunc {
 }
 
 const testAccAntiDdosV1DataSource_basic = `
+resource "opentelekomcloud_vpc_eip_v1" "eip_1" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name = "test"
+    size = 8
+    share_type = "PER"
+    charge_mode = "traffic"
+  }
+}
+
+resource "opentelekomcloud_antiddos_v1" "antiddos_1" {
+  floating_ip_id = "${opentelekomcloud_vpc_eip_v1.eip_1.id}"
+  enable_l7 = true
+  traffic_pos_id = 1
+  http_request_pos_id = 2
+  cleaning_access_pos_id = 1
+  app_type_id = 0
+}
+
 data "opentelekomcloud_antiddos_v1" "antiddos" {  
-  id = "7deb25d6-7a56-4d0b-9f28-305a069037d2"
+  floating_ip_id = "${opentelekomcloud_antiddos_v1.antiddos_1.id}"
 }
 `
